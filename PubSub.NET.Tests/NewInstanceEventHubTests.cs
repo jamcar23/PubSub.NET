2afab046388aab3b3933ba010ee8ddcf3b2994f0
James Carroll <jamcar23@gmail.com>
2020-01-21 19:21:47 -0600

    test(eventHub): add test for single instance event hub

---
+++ PubSub.NET.Tests/NewInstanceEventHubTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PubSubNET.Core;
+
+namespace PubSubNET.Tests
+{
+    public class NewInstanceEventHubTests : EventHubTests
+    {
+        protected override IEventHub EventHub => new EventHub();
+    }
+}
